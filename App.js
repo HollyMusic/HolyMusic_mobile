837b3a9de91b54aedf21ab97c541b5135045a054
Jose Nuñez <josnunezg@gmail.com>
2019-10-05 16:04:11 -0300

    add component and image

--- App.js
+++ App.js
@@ -1,19 +1,62 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppLoading } from 'expo';
+import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
+import * as Font from 'expo-font';
+import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import ChargingPage from './ChargingPage';
+
+const appColor = "#ffc917";
+
+class AnatomyExample extends React.Component {
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Left>
+            <Button transparent>
+              <Icon name='menu' />
+            </Button>
+          </Left>
+          <Body>
+            <Title>Holy Music</Title>
+          </Body>
+          <Right />
+        </Header>
+        <Content>
+          <Text>
+            Holy Music
+          </Text>
+        </Content>
+      </Container>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isReady: false,
+    };
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require('native-base/Fonts/Roboto.ttf'),
+      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+      ...Ionicons.font,
+    });
+    this.setState({ isReady: true })
+  }
+
+  render() {
+    if (!this.state.isReady) {
+      return <ChargingPage />;
+    }
+
+    return (
+      <AnatomyExample />
+    );
+  }
+}
