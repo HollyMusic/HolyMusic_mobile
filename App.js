6730bbbc723c5927c3222d6394856a2a2eb390c0
Jose Nuñez <josnunezg@gmail.com>
2019-10-06 11:58:07 -0300

    add principal view

--- App.js
+++ App.js
@@ -1,34 +1,108 @@
 import React from 'react';
-import { AppLoading } from 'expo';
-import { Container, Header, Title, Content, Footer, FooterTab, Button, Left, Right, Body, Icon, Text } from 'native-base';
-import * as Font from 'expo-font';
-import { Ionicons } from '@expo/vector-icons';
+import {
+  Image,
+  Dimensions,
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight
+} from 'react-native'
 
 import ChargingPage from './ChargingPage';
 
+const window = Dimensions.get('window');
+
 const appColor = "#ffc917";
+
+const feelings = [
+  "feliz",
+  "preocupado",
+  "animado",
+  "solo",
+  "con temor",
+  "resentido",
+  "impaciente",
+  "enamorado",
+  "confianza",
+  "tranquilo",
+  "enojado",
+  "miedo",
+  "compasión",
+  "gratitud"
+]
 
 class AnatomyExample extends React.Component {
   render() {
     return (
-      <Container>
-        <Header>
-          <Left>
-            <Button transparent>
-              <Icon name='menu' />
-            </Button>
-          </Left>
-          <Body>
-            <Title>Holy Music</Title>
-          </Body>
-          <Right />
-        </Header>
-        <Content>
-          <Text>
-            Holy Music
-          </Text>
-        </Content>
-      </Container>
+        <View style={{
+          height: window.height,
+          width: window.width,
+          backgroundColor: appColor,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center'
+          }}>
+          <Image
+            source={require('./assets/holy-music-logo.png')}
+            style={{
+              height: 100,
+              width: window.width - 50
+            }}
+          />
+          <View style={{alignItems: 'center', marginTop: 25}}>
+            <Text
+              style={{
+                fontSize: 50,
+                fontWeight: '200',
+                lineHeight: 50
+              }}
+            >Te damos la</Text>
+            <Text
+              style={{
+                fontSize: 40,
+                fontWeight: '700',
+                color: 'red',
+                lineHeight: 40
+              }}
+            >Bienvenida</Text>
+            <Text
+              style={{
+                fontSize: 45,
+                fontWeight: '100',
+                lineHeight: 50,
+                textTransform: 'uppercase',
+              }}
+            >José Melendez</Text>
+          </View>
+          <View style={{
+            borderRadius: 500,
+            backgroundColor: '#c2185b',
+            height: 150,
+            width: 150,
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center'
+          }}>
+            <Text style={{
+              color: 'white'
+            }}>¿Cómo te sientes?</Text>
+          </View>
+          <View style={{display: 'flex', flexDirection: 'row', maxWidth: '100%', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'center'}}>
+            <TouchableHighlight style={styles.badge} onPress={() => alert('hola')}>
+              <Text style={styles.badgeText}>Triste</Text>
+            </TouchableHighlight>
+            {
+              feelings.map((feeling, index) =>
+                <View
+                  key={index}
+                  style={styles.badge}
+                >
+                  <Text style={styles.badgeText}>{feeling}</Text>
+                </View>
+              )
+            }
+          </View>
+        </View>
     );
   }
 }
@@ -42,12 +116,9 @@ export default class App extends React.Component {
   }
 
   async componentDidMount() {
-    await Font.loadAsync({
-      Roboto: require('native-base/Fonts/Roboto.ttf'),
-      Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
-      ...Ionicons.font,
-    });
-    this.setState({ isReady: true })
+    setTimeout(() => {
+      this.setState({ isReady: true })
+    }, 2000)
   }
 
   render() {
@@ -60,3 +131,22 @@ export default class App extends React.Component {
     );
   }
 }
+
+
+const styles = StyleSheet.create({
+  badge: {
+    width: '30%',
+    paddingTop: 10,
+    backgroundColor: 'red',
+    margin: 3,
+    borderRadius: 1000,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  badgeText: {
+    color: 'white',
+    textTransform: 'uppercase',
+  }
+})
+
--- ChargingPage.js
+++ ChargingPage.js
@@ -2,26 +2,78 @@ import React from 'react';
 import {
   View,
   Image,
-  Dimensions
+  Dimensions,
+  StyleSheet,
+  Animated
 } from 'react-native';
-import {
-  Spinner
-} from 'native-base';
+
+const window = Dimensions.get("window");
 
 const image = require("./assets/holy-music-image.png");
+const logo = require("./assets/holy-music-logo.png");
 
 export default class ChargingPage extends React.Component {
+
+  state = {
+    opacityLogo: new Animated.Value(0.2)
+  }
+
+  _inOpacity = () => {
+    Animated.timing(this.state.opacityLogo, {
+      toValue: 1,
+      duration: 2000
+    }).start();
+  }
+
+  _outOpacity = () => {
+    Animated.timing(this.state.opacityLogo, {
+      toValue: 0.2,
+      duration: 2000
+    }).start();
+  }
+
+  _handleOpacity = () => {
+    this._outOpacity()
+    setTimeout(this._inOpacity, 2000);
+  }
+
+  componentDidMount() {
+    this._inOpacity();
+    setInterval(this._handleOpacity, 4000)
+  }
 
   render() {
     const { height, width } =  Dimensions.get('window');
     return (
-      <View style={{height, width, position: "relative"}}>
+      <View style={{position: "relative"}}>
         <Image
           style={{maxWidth: width, maxHeight: height}}
           source={image}
         />
-        <Spinner style={{position: "absolute", bottom: 5, right: 170 }} color="blue"/>
+        <Animated.View style={{...styles.container, opacity: this.state.opacityLogo}}>
+          <Image
+            style={{
+              width: width - 50,
+              height: 100
+            }}
+            source={logo}
+          />
+        </Animated.View>
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    position: "absolute",
+    zIndex: 4,
+    height: window.height,
+    width: window.width,
+    top: 0,
+    left: 0,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
